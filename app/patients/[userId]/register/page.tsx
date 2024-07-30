6d13c903254d333bdcd644b1b2c9286524a12305
Mikiyas Getachew <mikiyasget1234@gmail.com>
2024-07-30 13:18:54 +0100

    add patient detail information register page

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,36 @@
+import RegisterForm from "@/components/form/RegisterForm";
+import { getUser } from "@/lib/actions/patient";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const page = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+  // const patient = await getPatient(userId)
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container ">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <RegisterForm user={user} />
+          <p className="copyright py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
+    </div>
+  );
+};
+
+export default page;
